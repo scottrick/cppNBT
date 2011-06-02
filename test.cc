46f56a18898e798187c00433e288bcf9cf0e9e89
Lukas Niederbremer <Lukas.Niederbremer@hs-osnabrueck.de>
2011-06-02 02:59:19 +0200

    Fix missing space

--- test.cc
+++ test.cc
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2011 Lukas Niederbremer
-*
-* This file is part of cppNBT
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2011 Lukas Niederbremer
+ *
+ * This file is part of cppNBT
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <iostream>
 
 #include "src/cppnbt.h"
@@ -60,7 +60,7 @@ int main(int argc, char **argv)
     catch (GzipIOException &gzioe)
     {
         std::cerr << "Unable to load NBT file " << argv[1] << ": "
-        		  << gzioe.getCode() << std::endl;
+                  << gzioe.getCode() << std::endl;
     }
 
     return 0;
